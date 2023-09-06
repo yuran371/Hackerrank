c81c97afa2b8aa6c0918505f95bc11cf70cdc1fb
rosom <yuoran371@gmail.com>
2023-09-06 10:35:33 +0300

    Add unfinished project

--- hackerrank/JAVAsrc/Introduction/Java11_Int_to_String.java
+++ hackerrank/JAVAsrc/Introduction/Java11_Int_to_String.java
@@ -4,19 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Java11_Int_to _String {
+public class Java11_Int_to_String {
 
-    public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+    public static void main  (String[] args) throws IOException {
         try {
-        	int n=Integer.parseInt(reader.readLine().trim());
-        } catch (IOException e) {
-        	System.out.println(e.getMessage());
-        }
-        if (-100<=n&&n<=100) {
-        	Integer.toString() 
-        	
-        }
-        		
-    }
+        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+        String n =reader.readLine();
+        String s = Integer.toString(n);
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+        
+    } 
 }
+class DoNotTerminate {
+
+	 public static class ExitTrappedException extends SecurityException {
+
+	  private static final long serialVersionUID = 1;
+	 }
+
+	 public static void forbidExit() {
+	  final SecurityManager securityManager = new SecurityManager() {
+	   @Override
+	   public void checkPermission(Permission permission) {
+	    if (permission.getName().contains("exitVM")) {
+	     throw new ExitTrappedException();
+	    }
+	   }
+	  };
+	  System.setSecurityManager(securityManager);
+	 }
+	}
