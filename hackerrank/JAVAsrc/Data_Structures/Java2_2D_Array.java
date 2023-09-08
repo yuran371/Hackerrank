2e23971187d43fe3ee6d3cd6bdfcc7e11e5ef771
rosom <yuoran371@gmail.com>
2023-09-08 17:33:53 +0300

    Add finished project

--- hackerrank/JAVAsrc/Data_Structures/Java2_2D_Array.java
+++ hackerrank/JAVAsrc/Data_Structures/Java2_2D_Array.java
@@ -29,32 +29,33 @@ public class Java2_2D_Array {
         });   
         bufferedReader.close();
         Integer[][] array = new Integer[arr.size()][];
-        for (int i = 0; i < arr.size(); i++) {
-            List<Integer> row = arr.get(i);
-            array[i] = row.toArray(new Integer[row.size()]);
-        }
-        int a1=0;
-		int a2=0;
-		int a3=0;
-		int sum=-11110;
-        KVADRAT_OUTTER: for (int k=0; k<=3; k++) {
-        	
-        	KVADRAT_INNER: for (int l=0; l<=3; l++) {
-        		
-                PIRAM_OUTTER: for (int i=k; i<=2; i++) {
-          		  a1=array[i][l]+array[i][l+1]+array[i][l+2]+
-        				  +array[i+1][l+1]+
-        				  +array[i+2][l]+array[i+2][l+1]+array[i+2][l+2];
+       for (int i = 0; i < arr.size(); i++) {
+           List<Integer> row = arr.get(i);
+           array[i] = row.toArray(new Integer[row.size()]);
+       }
+       int a1=0;
+       int a2=0;
+       int a3=0;
+       int sum=Integer.MIN_VALUE;
+       KVADRAT_OUTTER: for (int i=0; i<4; i++) {
+           
+           KVADRAT_INNER: for (int l=0; l<4; l++) {
+               a1=0;
+               
 
-                  sum = Math.max(sum, a1);
-//                  if (sum<a1) {
-//                  	sum=a1;
-//                	}
+                         a1=array[i][l]+array[i][l+1]+array[i][l+2]+
+                                 +array[i+1][l+1]+
+                                 +array[i+2][l]+array[i+2][l+1]+array[i+2][l+2];
 
-                }
-        	}
-                
-        	}
-        System.out.println(sum);   	
+                         if (sum<a1) {
+                             sum=a1;
+                   }
+               
+
+
+           }
+               
+           }
+       System.out.println(sum);    	
     	}
     }
