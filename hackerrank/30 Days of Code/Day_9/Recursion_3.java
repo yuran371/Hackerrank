70e87feb7391d13187306f7bb6b18bf3bd93cf7b
rosom <yuoran371@gmail.com>
2023-09-17 23:42:49 +0300

    Add finished project (30 Days)

---
+++ hackerrank/30 Days of Code/Day_9/Recursion_3.java
@@ -0,0 +1,41 @@
+package Day_9;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Result {
+
+
+
+    public static int factorial(int n) {
+//    	if (n<0) {
+//    		return (Integer) null;
+//    	}
+//    	if (n==0) {
+//    		return 1;
+//    	}
+//    	if (n==1) {
+//    		return n;
+//    }
+//    	return n*factorial(n-1);
+    	return (n == 1) ? 1 : n*factorial(n-1) ; // альтернативное решение в одну строчку
+
+    }
+    
+}
+
+public class Recursion_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        System.out.println(result);
+    }
+}
