0944b0b6386055a0d11a3d92b0df1ed8e11b45ea
rosom <yuoran371@gmail.com>
2023-09-18 00:56:18 +0300

    Add UNfinished project (30 Days)

---
+++ hackerrank/30 Days of Code/Day_11/_2D_Arrays.java
@@ -0,0 +1,36 @@
+package Day_11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
+public class _2D_Arrays {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        int sum = 1;
+        for (int i=0; i<3; i++) {
+        	sum=arr.get(i)+arr.get(i+1)+arr.get(i+2);
+        }
+    }
+}
