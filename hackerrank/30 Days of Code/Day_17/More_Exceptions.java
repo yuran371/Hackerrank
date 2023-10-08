05600558fb94b4f42dfd5efdcee4c1607ab8b839
Artem Yuran <yuoran371@gmail.com>
2023-10-09 00:13:51 +0300

    Add finished project (30 Days of code)

---
+++ hackerrank/30 Days of Code/Day_17/More_Exceptions.java
@@ -0,0 +1,37 @@
+package Day_17;
+
+import java.util.Scanner;
+
+class Calculator {
+	int power (int n, int p) throws Exception {
+		int sum=1;
+		if (n>=0&&p>=0) {
+			for (int i=0; i<p; i++) {
+				sum*=n;
+			}
+	} else throw  new Exception("n and p should be non-negative");
+		return sum;
+	}
+}
+
+public class More_Exceptions {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
