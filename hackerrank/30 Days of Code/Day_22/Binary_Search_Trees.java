7373e74e85351f9e47cf7139ec2f12f0c76d18a6
Artem Yuran <yuoran371@gmail.com>
2023-10-11 00:20:08 +0300

    Try to understand how the locked stub code works

---
+++ hackerrank/30 Days of Code/Day_22/Binary_Search_Trees.java
@@ -0,0 +1,45 @@
+package Day_22;
+
+import java.util.Scanner;
+
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+public class Binary_Search_Trees {
+	public static int getHeight(Node root){
+      //Write your code here
+    }
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+	 public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
+    }
+}
