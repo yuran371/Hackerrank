ba3a9246ac4acf4ffd535000b2596766ce01e229
Artem Yuran <yuoran371@gmail.com>
2023-10-17 23:50:14 +0300

    Add finished project; repeated how to use try-w-res;

---
+++ hackerrank/JAVAsrc/Data_Structures/Java9_Map.java
@@ -0,0 +1,27 @@
+package Data_Structures;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Java9_Map {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		try (Scanner sc = new Scanner(System.in)) {
+			int n = sc.nextInt();
+			sc.nextLine();
+			for (int i=0; i<n; i++) {
+				String name = sc.nextLine();
+				int number = sc.nextInt();
+				map.put(name, number);
+				sc.nextLine();
+			}
+			while(sc.hasNext()) {
+				String s = sc.nextLine();
+				if (map.containsKey(s)) {
+					System.out.println(s+"="+map.get(s));
+				} else System.out.println("Not found");
+			}
+		}
+	}
+}
