ef69e14190820d14feba7c85f401bb1a0f53be3d
Artem Yuran <yuoran371@gmail.com>
2023-10-19 23:46:06 +0300

    Add finished project; implement Comparator<> interface

--- hackerrank/JAVAsrc/Data_Structures/Java5_Comparator.java
+++ hackerrank/JAVAsrc/Data_Structures/Java5_Comparator.java
@@ -1,11 +1,21 @@
 package Data_Structures;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-class Checker {
-	
+class Checker implements Comparator<Player> {
+	@Override
+	public int compare(Player p1, Player p2) {
+		if (p1.score==p2.score) {
+			return p1.name.compareTo(p2.name);
+		} else if (p1.score>p2.score) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
 }
 
 class Player{
