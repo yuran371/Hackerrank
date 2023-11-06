10b81ac065bdd7edbe65320aff6dbb11edceaea3
Artem Yuran <yuoran371@gmail.com>
2023-11-06 15:04:54 +0300

    Add four solutions for lambda's Calculus reductions from HackerRank

--- hackerrank/Functional Programming/Introduction/LambdaCalculusReductions.java
+++ hackerrank/Functional Programming/Introduction/LambdaCalculusReductions.java
@@ -1,31 +1,31 @@
 package Introduction;
 
-public class Lambda_Calculus_Reductions_4 {
+public class LambdaCalculusReductions {
 
 	public static void main(String[] args) {
 		
 //		Reduce the following expression to no more than one term. 
 //		If the expression cannot be reduced, enter "CAN'T REDUCE".
-		#1.
+		#1
 		((λx.(x y))(λz.z))
 		(λz.z) y
 		λz.z y
 		y		
 		
-		#2.
+		#2
 		((λx.((λy.(x y))x))(λz.w))
 		λx.((λy.(x y))x) λz.w
 		λy.(λz.w y) λz.w
 		λz.w λz.w
 		w // т.к. в теле w нет аргументов z
 		
-		#3.
+		#3
 		((λx.(x x))(λx.(x x))) 
 		λx.(x x) λx.(x x) 
 		λx.(x x) λx.(x x)
 		// infinity loop
 		
-		#4.
+		#4
 		λg.((λf.((λx.(f (x x)))(λx.(f (x x))))) g) 
 		λg.(g ((λx.(g (x x))) (λx.(g (x x)))))
 		// infinity loop		
